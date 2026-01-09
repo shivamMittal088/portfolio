326ad57fec09bc48f4353800ad2e19eff7864b61
shivamMittal088 <shivamofficial.sm@gmail.com>
2026-01-09 21:37:26 +0530

    `Added react-router-dom and routing configuration to App.tsx`

--- my-portfolio/my-app/src/App.tsx
+++ my-portfolio/my-app/src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router'
 
 function App() {
- 
+  // create routing paths here .
 
   return (
     <>
-      
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/about" element={<div>About</div>} />
+          <Route path="/projects" element={<div>Projects</div>} />
+          <Route path="/contact" element={<div>Contact</div>} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
